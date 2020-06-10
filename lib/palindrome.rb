d3ea1c6ebc178cdb9fba67bfbd5a61ba91910971
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-06-10 18:56:10 -0400

    small readability improvement

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -2,8 +2,7 @@
 # @return {Boolean}
 def is_palindrome(x)
   digits = x.to_s.chars
-  reverse = digits.reverse
-  reverse.zip(digits).each_with_index do |each, index|
+  digits.reverse.zip(digits).each_with_index do |each, index|
     return false if each[0] != each[1]
     return true if index >= digits.length / 2
   end

77da409099295f2d4d57dd5baf8fa2b0343e2ec1
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-06-10 19:01:44 -0400

    tiny improvement if we break early

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,6 +1,8 @@
 # @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
+  return false if x < 0
+
   digits = x.to_s.chars
   digits.reverse.zip(digits).each_with_index do |each, index|
     return false if each[0] != each[1]
